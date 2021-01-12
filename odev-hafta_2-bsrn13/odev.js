3ad87183120857f3733fc9e11d56d26e48a9f795
Büşra Şirin <72818581+bsrn13@users.noreply.github.com>
2021-01-12 19:01:14 +0300

    Add files via upload

---
+++ odev-hafta_2-bsrn13/odev.js
@@ -0,0 +1,92 @@
+/*
+  Odev 1:
+  Asagidaki fonksiyonu "Hello, John ciktisi verecek sekilde duzenleyiniz."
+ */
+
+var person = {
+  name: "John",
+  message: function () {    
+    console.log("Hello, " + this.name)
+  }
+}
+
+var messageFunc = person.message.bind(person);
+messageFunc();
+
+
+/*  
+  Odev 2:
+  Asagidaki fonksiyonu sirasiyla 
+  20
+  40
+  60
+  sonuclarini yazdiracak sekilde duzenleyiniz.
+*/
+var numbers = {
+  numbers: [[10,20,30], 2],
+  multiply: function(){
+    this.numbers[0].map((number) => {
+        const result = number * this.numbers[1];
+        console.log(result)
+    })
+  }
+};
+
+numbers.multiply();
+
+
+/* 
+  Odev 3:
+  Asagidaki isValidName fonksiyonunda bir isim validasyonu yaziniz.
+  Ipucu: Verilen arguman gecerli bir isim ise true degilse false donmeli
+  Ornek : isValidName("John") true donmeli
+  Ornek : isValidName(" J ohn") false donmeli
+*/
+function isValidName(name){
+   let a = name;
+    if (typeof(name) === "string") {
+      let trimmedName = name.trim().split(" ");
+       console.log(trimmedName)
+      if (trimmedName.length > 1 ||trimmedName[0].length < 2) {
+          return false;
+      }
+      return true;
+    }
+    else {
+        return false;
+    }
+    
+    }
+}
+
+/*
+  Odev 4:
+  Asagidaki katilimSaati fonksionu 2 arguman almaktadir.
+  dersSayisi: ogrencinin katildigi ders sayisi
+  dersSaati: her bir dersin dakika cinsinden suresi
+  katilimSaati fonkisyonu kac derse gerilidigi ve her bir dersin kac dakika surdugu bilgisini alip sonuc olarak toplamda kac dakika derse girildigini donmelidir.
+  - iki arguman de number veya string olarak verilebilir.
+  - Sayi olmayan ya da sayiya cevrilemeyen argumanlar var ise hata donulmelidir.
+  Ornek: katilimSaati(3, 30) 90 sonucunu vermelidir.
+  Ornek: katilimSaati("3", 20) 60 sonucunu vermelidir.
+  Ornek: katilimSaati("5", "30") 150 sonucunu vermelidir.
+*/
+function katilimSaati(dersSayisi, dersSuresi){
+  let a = dersSayisi * dersSuresi;
+  let b = String(dersSayisi);
+  let c = String(dersSuresi);
+    if (parseInt(b) && parseInt(c)) {
+      console.log(a)
+    return true;  
+  } 
+  else {
+    return false;
+  }
+ 
+}
+
+
+}
+
+
+
